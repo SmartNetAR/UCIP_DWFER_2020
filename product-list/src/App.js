683abe393b01e7f197b38790cf17a1cc3a185021
Leonardo Casales <leonardo@smartnet.com.ar>
2020-06-17 20:09:26 -0300

    fetch products

--- product-list/src/App.js
+++ product-list/src/App.js
@@ -12,38 +12,16 @@ class App extends React.Component {
                 producto: '',
                 cantidad: 0,
             },
-            productos: [
-                {
-                id: 0,
-                nombre: "banana",
-                cantidad: 10
-                },
-                {
-                id: 1,
-                nombre: "manzana",
-                cantidad: 0
-                },
-                {
-                id: 2,
-                nombre: "naranja",
-                cantidad: 5
-                },
-                {
-                id: 3,
-                nombre: "durazno",
-                cantidad: 1
-                },
-                {
-                id: 4,
-                nombre: "anana",
-                cantidad: 0
-                }
-            ]
+            productos: []
         }
 
         this.handleChangeForm = this.handleChangeForm.bind(this);
     }
     
+
+    componentDidMount() {
+        this.getProducts();
+    }
 
 
     handleChangeForm(e) {
@@ -64,18 +42,45 @@ class App extends React.Component {
     
     productosAReponerStock = () => this.state.productos.filter(producto => producto.cantidad === 1)
 
-    handleIncrement = ( parametro ) => {
+    handleAddProduct = ( parametro ) => {
 
-        const nuevaListaProductos = this.state.productos; 
-        nuevaListaProductos.push({
-            id: nuevaListaProductos.length,
-            nombre: this.state.formValues.producto,
-            cantidad:  parseInt( this.state.formValues.cantidad, 10 )
+        if ( this.state.formValues.id ) {
+            alert("editando")
+            
+        } else {
+            const nuevaListaProductos = this.state.productos; 
+            nuevaListaProductos.push({
+                id: nuevaListaProductos.length,
+                nombre: this.state.formValues.producto,
+                cantidad:  parseInt( this.state.formValues.cantidad, 10 )
+            })
+    
+            this.setState({ productos: nuevaListaProductos});
+    
+            console.log( this.state.productos );
+
+        }
+    }
+
+    handleEdit = ( item ) => {
+        console.log(item);
+
+        this.setState({
+            formValues: {
+                id: item.id,
+                producto: item.nombre,
+                cantidad: item.cantidad,
+            },
         })
 
-        this.setState({ productos: nuevaListaProductos});
 
-        console.log( this.state.productos );
+    }
+
+    getProducts = async () => {
+        const respuesta = await fetch("http://localhost:3000/productos.json");
+        const datos = await respuesta.json()
+        console.log(datos);
+        this.setState( {productos:datos })
     }
 
     render() {
@@ -84,11 +89,11 @@ class App extends React.Component {
           <div className="App">
             <header className="App-header">
                 <FormProducto
-                    onAddProduct={ this.handleIncrement }
+                    onAddProduct={ this.handleAddProduct }
                     formValues={this.state.formValues}
                     handleChange={ this.handleChangeForm }
                 />
-              <ListaProductos titulo="Productos en stock" productos={this.productosEnStock()} />
+              <ListaProductos titulo="Productos en stock" productos={this.productosEnStock()} onClick={ this.handleEdit }/>
               <ListaProductos titulo="Productos sin stock" productos={this.productosSinStock()} />
               <ListaProductos titulo="Productos a reponer" productos={this.productosAReponerStock()} />
               <ListaProductos titulo="Productos en oferta" productos={this.state.productos.filter( producto => producto.cantidad >= 5 )} />
