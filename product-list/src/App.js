a3412a6d9b46a44dfab58d45d1e669683cb7341c
Leonardo Casales <leonardo@smartnet.com.ar>
2020-07-08 19:57:56 -0300

    product list pasado de clase a funcion y uso de hooks useState y useEffect

--- product-list/src/App.js
+++ product-list/src/App.js
@@ -1,107 +1,95 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import ListaProductos from './components/ListaProductos';
 import FormProducto from './components/FormProducto';
 
-class App extends React.Component {
+function App() {
 
-    constructor () {
-        super();
-        this.state = {
-            formValues: {
-                producto: '',
-                cantidad: 0,
-            },
-            productos: []
-        }
-
-        this.handleChangeForm = this.handleChangeForm.bind(this);
-    }
+    const [formValues, setFormValues] = useState({
+        producto: '',
+        cantidad: 0,
+    });
     
+    const [productos, setProductos] = useState([]);
 
-    componentDidMount() {
-        this.getProducts();
-    }
+    useEffect(() => {
+
+        const getProducts = async () => {
+            const respuesta = await fetch("http://localhost:3000/productos.json");
+            const datos = await respuesta.json()
+            console.log(datos);
+            setProductos(datos)
+        }
+        
+        getProducts();
+
+    }, [])
 
 
-    handleChangeForm(e) {
+    const handleChangeForm = (e) => {
 
         const { name, value } = e.target;
 
-        this.setState( {
-            formValues: {
-                ...this.state.formValues ,
+
+            setFormValues({
+                ...formValues ,
                 [name] : value
-            }
-        })
+            })
     }
 
-    productosEnStock = () => this.state.productos.filter(producto => producto.cantidad > 0)
+    const productosEnStock = () => productos.filter(producto => producto.cantidad > 0)
 
-    productosSinStock = () => this.state.productos.filter(producto => producto.cantidad === 0)
+    const productosSinStock = () => productos.filter(producto => producto.cantidad === 0)
     
-    productosAReponerStock = () => this.state.productos.filter(producto => producto.cantidad === 1)
+    const productosAReponerStock = () => productos.filter(producto => producto.cantidad === 1)
 
-    handleAddProduct = ( parametro ) => {
+    const handleAddProduct = ( parametro ) => {
 
-        if ( this.state.formValues.id ) {
+        if ( formValues.id ) {
             alert("editando")
             
         } else {
-            const nuevaListaProductos = this.state.productos; 
+            const nuevaListaProductos = [...productos]; 
             nuevaListaProductos.push({
                 id: nuevaListaProductos.length,
-                nombre: this.state.formValues.producto,
-                cantidad:  parseInt( this.state.formValues.cantidad, 10 )
+                nombre: formValues.producto,
+                cantidad:  parseInt( formValues.cantidad, 10 )
             })
     
-            this.setState({ productos: nuevaListaProductos});
-    
-            console.log( this.state.productos );
+            setProductos( nuevaListaProductos );
 
         }
     }
 
-    handleEdit = ( item ) => {
+    const handleEdit = ( item ) => {
         console.log(item);
 
-        this.setState({
-            formValues: {
-                id: item.id,
-                producto: item.nombre,
-                cantidad: item.cantidad,
-            },
+        setFormValues({
+            id: item.id,
+            producto: item.nombre,
+            cantidad: item.cantidad,
         })
 
-
     }
 
-    getProducts = async () => {
-        const respuesta = await fetch("http://localhost:3000/productos.json");
-        const datos = await respuesta.json()
-        console.log(datos);
-        this.setState( {productos:datos })
-    }
 
-    render() {
 
-        return (
-          <div className="App">
-            <header className="App-header">
-                <FormProducto
-                    onAddProduct={ this.handleAddProduct }
-                    formValues={this.state.formValues}
-                    handleChange={ this.handleChangeForm }
-                />
-              <ListaProductos titulo="Productos en stock" productos={this.productosEnStock()} onClick={ this.handleEdit }/>
-              <ListaProductos titulo="Productos sin stock" productos={this.productosSinStock()} />
-              <ListaProductos titulo="Productos a reponer" productos={this.productosAReponerStock()} />
-              <ListaProductos titulo="Productos en oferta" productos={this.state.productos.filter( producto => producto.cantidad >= 5 )} />
-            </header>
-          </div>
-        );
+    return (
+        <div className="App">
+        <header className="App-header">
+            <FormProducto
+                onAddProduct={ handleAddProduct }
+                formValues={formValues}
+                handleChange={ handleChangeForm }
+            />
+            <ListaProductos titulo="Productos en stock" productos={productosEnStock()} onClick={ handleEdit }/>
+            <ListaProductos titulo="Productos sin stock" productos={productosSinStock()} />
+            <ListaProductos titulo="Productos a reponer" productos={productosAReponerStock()} />
+            <ListaProductos titulo="Productos en oferta" productos={productos.filter( producto => producto.cantidad >= 5 )} />
+        </header>
+        </div>
+    );
 
-    }
 }
 
 export default App;
